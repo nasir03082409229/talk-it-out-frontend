1cd499554f4f033423f70a5f955e455db98d84a0
nasir03082409229 <nasir03082409229@gmail.com>
2021-04-16 06:45:14 +0500

    Fix: Test screen

--- src/Screens/TestScreen/index.js
+++ src/Screens/TestScreen/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native'
 import Video from 'react-native-video'
+import TrackPlayer from 'react-native-track-player';
 
 
 const TestScreen = () => {
+    useEffect(() => {
+        start();
+    }, [])
     return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Video
-            source={{ uri: 'https://aud1.sjamz.com/9111/stream' }}
-            // audioOnly={true}
-            playInBackground={true}
-        />
         <Text>
             GO TO HOME
         </Text>
     </View>)
 }
+
+const start = async () => {
+    // Set up the player
+    await TrackPlayer.setupPlayer();
+
+    // Add a track to the queue
+    await TrackPlayer.add({
+        id: 'trackId',
+        url: 'https://aud1.sjamz.com/9111/stream',
+        title: 'Track Title',
+        artist: 'Track Artist',
+    });
+
+    // Start playing it
+    await TrackPlayer.play();
+};
+
 
 export default TestScreen
